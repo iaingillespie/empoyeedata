f08fa6461b7e4fa7d57531de9a0959f40f3ed82f
alexszeliga <alexszeliga@gmail.com>
2018-09-13 18:58:44 -0400

    init js file

---
+++ script.js
@@ -0,0 +1,12 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDdOzgW64h0XpCasrBLA1MpedTJkbZ1Kro",
+  authDomain: "employee-database-ff411.firebaseapp.com",
+  databaseURL: "https://employee-database-ff411.firebaseio.com",
+  projectId: "employee-database-ff411",
+  storageBucket: "",
+  messagingSenderId: "1005544872924"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
